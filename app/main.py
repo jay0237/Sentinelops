a8d529b4febd0e86d1742e012ce2f50c0706a8d9
jay0237 <jayjoshi16534@gmail.com>
2026-05-11 20:53:51 +0530

    Intial FastAPI Setup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from app.config.database import engine
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
-    return{"message": "SentinelOps API is Running"}
+    return {
+        "message": "SentinelOps API is Running"
+    }
+
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "healthy",
+        "service": "SentinelOps API",
+        "version": "1.0.0"
+    }
+
+
+@app.get("/db-check")
+def db_check():
+    try:
+        connection = engine.connect()
+        connection.close()
+
+        return {
+            "database": "connected"
+        }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
