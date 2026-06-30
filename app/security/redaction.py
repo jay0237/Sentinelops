eb9d1e4d042193cb58f2626abf683d56d5959aa3
jay0237 <jayjoshi16534@gmail.com>
2026-06-30 20:48:31 +0530

    .

--- app/security/redaction.py
+++ app/security/redaction.py
@@ -1,13 +1,15 @@
 import re
 
-EMAIL_REGEX= r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-PHONE_REGEX =  r"\b\d{10}\b"
+EMAIL_REGEX = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+PHONE_REGEX = r"\b\d{10}\b"
+
 
 def redact_pii(text):
 
-    text = re.sub(EMAIL_REGEX,
-    "[EMAIL_REDACTED]",
-    text
+    text = re.sub(
+        EMAIL_REGEX,
+        "[EMAIL_REDACTED]",
+        text
     )
 
     text = re.sub(
