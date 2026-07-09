e463cc9f04f6059db9faa8486c1e323062c3c3b0
jay0237 <jayjoshi16534@gmail.com>
2026-07-09 23:22:20 +0530

    add update rule

--- app/routes/threat_rules.py
+++ app/routes/threat_rules.py
@@ -46,3 +46,22 @@ def create_rule( rule: ThreatRuleCreate, db: Session = Depends(get_db)):
                 "message" : "Rule Deleted Successfully"
 
             }
+
+    @router.put("/{rule_id}")
+    def update_rule( rule_id: int, rule: ThreatRuleCreate, db: Session = Depends(get_db)):
+
+        db_rule = db.query(ThreatRule).filter(ThreatRule.id == rule_id).first()
+
+        if not db_rule:
+            return {"message": "Rule not found"}
+
+            db_rule.keyword = rule.keyword
+            db_rule.category = rule.category
+            db_rule.severity = rule.severity
+            db_rule.reason = rule.reason
+            db_rule.is_active = rule.is_active
+
+            db.commit()
+            db.refresh(db_rule)
+
+            return db_rule
